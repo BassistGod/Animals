b4ff0bf084affed2395844772a4128bf999bcb85
Admin <Vodopianova_19a8@mail.ru>
2022-04-29 20:27:47 +0700

    add create record with tags

--- admin_create.php
+++ admin_create.php
@@ -9,18 +9,37 @@ if (isset($_COOKIE['bd_create_success']) AND $_COOKIE['bd_create_success'] !='')
     } 
 }
 
+// создаем список для выбора тегов 
+$mysql = connect();
+$tag = getAllTags($mysql);
+close ($mysql);
+$out = '<p>Tags: <select name="tag[]" multiple="multiple">';
+for ($i=0; $i < count($tag); $i++){
+    $out .= "<option>{$tag[$i]}</option>";
+}
+$out .= "</select></p>";
+
+// добавляем записи в базу данных
 if (isset($_POST['title']) AND $_POST['title'] !=''){
     $title = $_POST['title'];
     $descr_min = $_POST['descr_min'];
     $description = $_POST['description'];
+    $newTags = $_POST['tag'];
 
     move_uploaded_file($_FILES['image']['tmp_name'], 'images/'.$_FILES['image']['name']);
 
     $mysql = connect();
     $sql = "INSERT INTO info (title, descr_min, description, image)
     VALUES ('$title', '$descr_min', '$description', '".$_FILES['image']['name']."')";
-
+   
     if ($mysql->query($sql) === TRUE) {
+        if (isset($newTags) AND $newTags !=''){
+            $lastId = $mysql->insert_id;
+            for ($i=0; $i < count($newTags); $i++){
+                $sql = "INSERT INTO tag (tag, post) VALUES ('$newTags[$i]', '$lastId')";
+                $mysql->query($sql);
+            }
+        }
         header('Location: /New_lessons/14_animal/admin.php');
         setcookie("bd_create_success", '1', time()+10);
     } else {
@@ -37,7 +56,11 @@ if (isset($_POST['title']) AND $_POST['title'] !=''){
   <textarea name="descr_min" id="" cols="30" rows="10"></textarea>
   <p>Description:</p>
   <textarea name="description" id="" cols="30" rows="10"></textarea>
+  <?php
+  echo $out;
+  ?>
   <p>Photo: <input type="file" name="image"></p>
   <p><input type="submit" value="add"></p>
+  
 
 </form>
